15605d392f0730fd7b6197c289f3119fdd13b115
aycan acar <108690004+aycanz@users.noreply.github.com>
2022-12-26 22:38:58 +0300

    Add files via upload

---
+++ muksayi.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+// kendisi haric pozitif tam bölenlerin toplami kendisine eþit olan sayilara mük sayi denir.
+
+ int muksayi(int sayi){
+ 	int i,topla=0;
+ 	for(i=1;i<sayi;i++){
+ 		 if(sayi%i==0)
+ 		 topla+=i;
+	 }
+	 if (topla==sayi)
+	 return 1;
+	 else 
+	 return 0;
+ }
+
+int main(){
+	int sayi,sonuc;
+	printf("bir sayi girin");
+	scanf("%d",&sayi);
+	sonuc=muksayi(sayi);
+	if(sonuc==1)
+	printf("muk");
+	
+	else 
+	printf("muk degil");
+	
+	return 0;
+}
